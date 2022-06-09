e182b29a0a691861408e26e3e402422d253501c6
Alon Dikman <107044821+alondikman@users.noreply.github.com>
2022-06-09 17:41:48 +0300

    Add files via upload

---
+++ uninstall_scripts/dynamic unistall app.ps1
@@ -0,0 +1,36 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+
+Get-WmiObject -Class Win32_Product | Select-Object name, Version, Caption 
+$progname = Read-Host -Prompt 'please type the progrem to remove'
+$name = @("*$progname*")
+
+$remove = Get-Package -Provider Programs -IncludeWindowsInstaller -Name "$name" | Select-Object -ExpandProperty name
+
+Write-Warning "you select to remove $remove"
+
+$confirmation = Read-Host "Unistall $remove [y/n]"
+while($confirmation -ne "y")
+{
+    if ($confirmation -eq 'n') {exit}
+    $confirmation = Read-Host "Unistall $remove [y/n]"
+}
+
+    
+Uninstall-Package $remove
+
+$confirmation = Read-Host "continu to EXE removal [y/n]"
+while($confirmation -ne "y")
+{
+    if ($confirmation -eq 'n') {exit}
+    $confirmation = Read-Host "continu to EXE removal [y/n]"
+}
+
+#for EXE
+Install-Script -Name Get-ProgramUninstallString -force
+$string = Get-ProgramUninstallString -Name "$remove" | Select-Object -ExpandProperty UninstallString
+cmd /c $string /x /quiet /S
+
+pause
---
+++ uninstall_scripts/get unistall string for any app.ps1
@@ -0,0 +1,25 @@
+$appname = Read-Host -Prompt 'please type the progrem name'
+
+$productNames = @("*$appname*")
+$UninstallKeys = @('HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall',
+                    'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall',
+                    'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
+                    )
+$results = foreach ($key in (Get-ChildItem $UninstallKeys) ) {
+
+    foreach ($product in $productNames) {
+        if ($key.GetValue("DisplayName") -like "$product") {
+            [pscustomobject]@{
+                KeyName = $key.Name.split('\')[-1];
+                DisplayName = $key.GetValue("DisplayName");
+                UninstallString = $key.GetValue("UninstallString");
+                Publisher = $key.GetValue("Publisher");
+            }
+        }
+    }
+}
+
+$results
+Write-Output $results | puase
+
+pause
